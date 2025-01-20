ec8d5c6e2bac810463e00f5ada2483ae7e848d11
kkj860710 <kkj9491@knou.ac.kr>
2025-01-21 08:52:01 +0900

    기본 세션설정. 로그인 로그아웃

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,47 +1,55 @@
 import NextAuth, { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
 
 /**
  * NextAuthOptions 설정
  */
 export const authOptions: NextAuthOptions = {
   providers: [
+    CredentialsProvider({
+      // The name to display on the sign in form (e.g. "Sign in with...")
+      name: "Credentials",
+      // `credentials` is used to generate a form on the sign in page.
+      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+      // e.g. domain, username, password, 2FA token, etc.
+      // You can pass any HTML attribute to the <input> tag through the object.
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "Input Name" },
+        password: { label: "Password", type: "password" }
+      },
+      async authorize(credentials, req) {
+        // Add logic here to look up the user from the credentials supplied
+        const user = { id: "1", name: "J Smith", email: "jsmith@example.com" }
 
+        if (user) {
+          // Any object returned will be saved in `user` property of the JWT
+          return user
+        } else {
+          // If you return null then an error will be displayed advising the user to check their details.
+          return null
+
+          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+        }
+      }
+    })
   ],
   session: {
     strategy: "jwt",
   },
   callbacks: {
-    /**
-     * 로그인(signIn) 시 실행되는 콜백
-     */
-    async signIn() {
-
-      return true; // true면 로그인 허용
+    async signIn({ user, account, profile, email, credentials }) {
+      return true
     },
-
-    /**
-     * JWT 생성/갱신 시 실행되는 콜백
-     */
-    async jwt({ token , account }) {
-      // 로그인 시점에 account가 존재하므로, 해당 내용을 토큰에 추가
-      if (account) {
-        token.accessToken = account.access_token;
-        token.provider = account.provider;
-      }
-      return token;
+    async redirect({ url, baseUrl }) {
+      return baseUrl
     },
-
-    /**
-     * 세션 객체 생성/갱신 시 실행되는 콜백
-     */
-    async session({ session }) {
-      // token에 저장된 accessToken, provider를 session에 전달
-
-     return session;
-
+    async session({ session, user, token }) {
+      return session
     },
-  },
-};
+    async jwt({ token, user, account, profile, isNewUser }) {
+      return token
+    },
+}};
 
 /**
  * NextAuth 핸들러
--- src/components/common/NavItem.tsx
+++ src/components/common/NavItem.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import Link from "next/link";
+import {signIn, signOut, useSession} from "next-auth/react";
 
 
 const NavItem = ( { mobile }  : {mobile?: boolean}) => {
+    const {data: session, status} = useSession();
+    console.log(session);
+    console.log(status);
     return (
         <ul className={`text-md justify-center flex gap-4 w-full items-center ${mobile && "flex-col h-full"}`}>
 
             <li className="py-2 text-center border-b-4 cursor-pointer"><Link href="/admin">Admin</Link></li>
-            <li className="py-2 text-center border-b-4 cursor-pointer"><Link href="/admin">Admin</Link></li>
-            <li className="py-2 text-center border-b-4 cursor-pointer"><button>Sign Out</button></li>
-            <li className="py-2 text-center border-b-4 cursor-pointer"><button>Sign In</button></li>
+            <li className="py-2 text-center border-b-4 cursor-pointer"><Link href="/user">User</Link></li>
+            {session?.user ? (
+                <li className="py-2 text-center border-b-4 cursor-pointer">
+                    <button onClick={() => signOut()}>Sign Out</button>
+                </li>
+            ) : (
+                <li className="py-2 text-center border-b-4 cursor-pointer">
+                    <button onClick={() => signIn()}>Sign In</button>
+                </li>
+            )}
         </ul>
     )
 }
